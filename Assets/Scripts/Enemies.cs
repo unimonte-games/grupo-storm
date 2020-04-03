aedd33a17085584bf7488d9e0e9585b5c952821c
Guilherme <nuequiz@hotmail.com>
2020-04-02 21:23:58 -0300

    Projeto Iniciado

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+   public Transform spawnnPoint;
+   public GameObject obsPrefab;
+   public float maxTime;
+   public float minTime;
+   private float time;
+   private float spawnTime;
+
+   void Start ()
+   {
+       time = minTime;
+   }
+
+   void FixedUpdate ()
+   {
+       SetRandomTime ();
+       time += Time.deltaTime;
+
+       if (time >= spawnTime)
+       {
+           SpawnObject();
+           SetRandomTime();
+       }
+   }
+
+   void SpawnObject ()
+   {
+       time = minTime;
+       StartCoroutine(Example());
+       Instantiate (obsPrefab, spawnnPoint.position, spawnnPoint.rotation);
+   }
+
+   void SetRandomTime ()
+   {
+       spawnTime = Random.Range (minTime, maxTime);
+   }
+
+   IEnumerator Example ()
+   {
+       yield return new WaitForSeconds (200f);
+   }
+}
